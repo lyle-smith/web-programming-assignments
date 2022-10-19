b668de9db7bd00a59df4f2f1690eeec86c57e4ec
lyle-smith <smithl14@newpaltz.edu>
2022-10-18 22:06:05 -0400

    Added Staticfile and updated app to match classwork

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,59 @@
+<!-- eslint-disable prettier/prettier -->
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { RouterLink } from 'vue-router';
+  import LoginBadge from './LoginPage.vue';
+
+  let isActive = ref(false);
+</script>
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item has-text-weight-semibold" href="https://bulma.io"
+          ><span class="primary">SPE</span>CIAL</a
+        >
+
+        <a
+          :class="{ 'is-active': isActive }"
+          @click="isActive = !isActive"
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': isActive }"
+      >
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">Home</router-link>
+          <router-link to="/shop" class="navbar-item">Shop</router-link>
+          <router-link to="/news" class="navbar-item">News</router-link>
+          <router-link to="/support" class="navbar-item">Support</router-link>
+          <div class="navbar-item">
+            <div class="buttons">
+              <LoginBadge />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+.router-link-active {
+  border-bottom: #df0a0a 2px solid;
+}
+.primary {
+  color: #df0a0a;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container-fluid columns is-centered is-vcentered">
+    <div class="column is-6">
+      <h2 class="title is-1 secondary"><span class="has-text-danger">WORKOUT</span> TIME</h2>
+      <h3 class="subtitle is-3 secondary">SPECIAL CLASSES</h3>
+      <button class="button is-rounded px-6 mr-3 is-danger">Explore</button>
+      <button class="button is-rounded px-6">Read More</button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  background-image: url(../assets/landing-background.jpg);
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  min-height: 100vh;
+  color: white;
+}
+
+.primary {
+  color: #e40215;
+}
+.primary-background {
+  background-color: #e40215;
+}
+
+.secondary {
+  color: #f5f5f5;
+}
+</style>
